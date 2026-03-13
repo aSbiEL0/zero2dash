7adbad534412c87b3fed8526da3ebf4df37cacd5
asbiel0 <asbiel0@gmail.com>
2026-03-13 23:56:33 +0000

    ok

--- reload.sh
+++ reload.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+set -eu
+
 cd /home/pihole/zero2dash/
-echo Refreshing all modules and rendering new images where necessary
-sleep 0.1
-systemctl /etc/systemd/system/daemon-reload
-sleep 1
-Daemon-reload completed
+echo "Refreshing all modules and rendering new images where necessary"
+
+systemctl daemon-reload
+echo "Daemon-reload completed"
+
 python3 /home/pihole/zero2dash/modules/calendash/calendash-api.py
 python3 /home/pihole/zero2dash/modules/currency/currency-rate.py --force-refresh
 python3 /home/pihole/zero2dash/modules/weather/weather_refresh.py
-echo Images rendered, reloading display.service to test output. 
-systemctl reload display.service
-echo Task complete.
+
+echo "Images rendered, restarting display.service to test output."
+systemctl restart display.service
+echo "Task complete."
